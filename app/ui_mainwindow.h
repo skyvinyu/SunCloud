be11854f35a2231b09990a9003f28b1e5d81a5bd
Skyvin <skyvin.yu@gmail.com>
2015-01-26 12:50:32 +0100

    Move header files to src and delete include directory, move ui/ui_mainwindow.h to app

---
+++ app/ui_mainwindow.h
@@ -0,0 +1,99 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created: Mon 26. Jan 12:40:56 2015
+**      by: Qt User Interface Compiler version 4.8.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHeaderView>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QStatusBar>
+#include <QtGui/QToolBar>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionAbout_SunCloud;
+    QAction *actionHelp;
+    QAction *actionQuit;
+    QAction *actionPreference;
+    QWidget *centralWidget;
+    QMenuBar *menuBar;
+    QMenu *menuSunCloud;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(400, 300);
+        actionAbout_SunCloud = new QAction(MainWindow);
+        actionAbout_SunCloud->setObjectName(QString::fromUtf8("actionAbout_SunCloud"));
+        actionHelp = new QAction(MainWindow);
+        actionHelp->setObjectName(QString::fromUtf8("actionHelp"));
+        actionQuit = new QAction(MainWindow);
+        actionQuit->setObjectName(QString::fromUtf8("actionQuit"));
+        actionPreference = new QAction(MainWindow);
+        actionPreference->setObjectName(QString::fromUtf8("actionPreference"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 400, 21));
+        menuSunCloud = new QMenu(menuBar);
+        menuSunCloud->setObjectName(QString::fromUtf8("menuSunCloud"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuSunCloud->menuAction());
+        menuSunCloud->addAction(actionPreference);
+        menuSunCloud->addAction(actionAbout_SunCloud);
+        menuSunCloud->addAction(actionHelp);
+        menuSunCloud->addSeparator();
+        menuSunCloud->addAction(actionQuit);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
+        actionAbout_SunCloud->setText(QApplication::translate("MainWindow", "About SunCloud", 0, QApplication::UnicodeUTF8));
+        actionHelp->setText(QApplication::translate("MainWindow", "Help", 0, QApplication::UnicodeUTF8));
+        actionQuit->setText(QApplication::translate("MainWindow", "Quit", 0, QApplication::UnicodeUTF8));
+        actionPreference->setText(QApplication::translate("MainWindow", "Preference", 0, QApplication::UnicodeUTF8));
+        menuSunCloud->setTitle(QApplication::translate("MainWindow", "SunCloud", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
